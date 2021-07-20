1f2703ce49d41788fe300d3a79218f6c52e5ee0d
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2021-07-20 13:10:04 -0300

    responsible sidebar and dashboard

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,45 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
-import { Logo } from './Logo'
-import { NotificationsNav } from './NotificationsNav'
-import { Profile } from './Profile'
-import { SearchBox } from './SearchBox'
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
+import { Logo } from "./Logo";
+import { NotificationsNav } from "./NotificationsNav";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
 
 export function Header() {
-    const isWideVersion = useBreakpointValue({
-        base: false,
-        lg: true
-    })
+  const { onOpen } = useSidebarDrawer();
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-    return (
-        <Flex w="100%" as="header" maxWidth={1480} h="20" mx="auto" mt="4" align="center" px="6">
-            <Logo />
-            {isWideVersion && <SearchBox />}
-            <Flex align="center" ml="auto">
-                <NotificationsNav />
-                <Profile showProfileData={isWideVersion} />
-            </Flex>
-        </Flex>
-    )
+  return (
+    <Flex
+      w="100%"
+      as="header"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      align="center"
+      px="6"
+    >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Abrir menu"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
+      <Logo />
+      {isWideVersion && <SearchBox />}
+      <Flex align="center" ml="auto">
+        <NotificationsNav />
+        <Profile showProfileData={isWideVersion} />
+      </Flex>
+    </Flex>
+  );
 }
