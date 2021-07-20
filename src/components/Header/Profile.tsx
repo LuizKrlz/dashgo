3efc35365655d85a8f549acf8ef9ffc16dc5b78a
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2021-07-20 09:33:36 -0300

    responsible on header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,18 @@
 import { Text, Box, Flex, HStack, Icon, Avatar } from "@chakra-ui/react";
-import { RiNotificationLine, RiUserAddLine } from "react-icons/ri";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps ) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text >Luiz Carlos</Text>
-                <Text color="gray.300" fontSize="small">luiz.correa.dev@gmail.com</Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text >Luiz Carlos</Text>
+                    <Text color="gray.300" fontSize="small">luiz.correa.dev@gmail.com</Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Luiz Carlos" src="https://github.com/luizkrlz.png" />
         </Flex>
