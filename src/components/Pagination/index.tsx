18c230704292a6437440600f63973ef7dc9842cf
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2021-07-20 13:25:12 -0300

    responsible sidebar and dashboard

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,25 +1,25 @@
 import { Box, Button, Stack } from "@chakra-ui/react";
-import {PaginationItem} from './PaginationItem'
+import { PaginationItem } from "./PaginationItem";
 
 export function Pagination() {
-    return (
-        <Stack
-            spacing="6"
-            direction="row"
-            mt="8"
-            justify="space-between"
-            align="center"
-        >
-            <Box>
-                <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
-            </Box>
-            <Stack direction="row" spacing="2">
-                <PaginationItem  number={1} isCurrent />
-                <PaginationItem  number={2} />
-                <PaginationItem  number={3} />
-                <PaginationItem  number={4} />
-                <PaginationItem  number={5} />
-            </Stack>
-        </Stack>
-    )
+  return (
+    <Stack
+      spacing="6"
+      direction={["column", "row"]}
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <Stack direction="row" spacing="2">
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+      </Stack>
+    </Stack>
+  );
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,98 +1,148 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-    return (
-        <Box>
-            <Header />
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-                <Sidebar />
+  return (
+    <Box>
+      <Header />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-                    <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
-                            Criar novo
-                        </Button>
-                    </Flex>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px="6" color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>
-                                    Usuário
-                                </Th>
-                                <Th>Data de cadastro</Th>
-                                <Th w="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Luiz Carlos</Text>
-                                        <Text fontSize="sm" color="gray.300">luiz.correa.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>04 de abril 2021</Td>
-                                <Td>
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
 
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Luiz Carlos</Text>
-                                        <Text fontSize="sm" color="gray.300">luiz.correa.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>04 de abril 2021</Td>
-                                <Td>
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
+          <Table colorScheme="whiteAlpha" overflowX="scroll">
+            <Thead>
+              <Tr>
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Luiz Carlos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      luiz.correa.dev@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de abril 2021</Td>}
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
 
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Luiz Carlos</Text>
-                                        <Text fontSize="sm" color="gray.300">luiz.correa.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>04 de abril 2021</Td>
-                                <Td>
-                                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
-                </Box>
-            </Flex>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Luiz Carlos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      luiz.correa.dev@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de abril 2021</Td>}
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Luiz Carlos</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      luiz.correa.dev@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                {isWideVersion && <Td>04 de abril 2021</Td>}
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
         </Box>
-    )
+      </Flex>
+    </Box>
+  );
 }
